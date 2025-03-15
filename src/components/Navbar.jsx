ece38cbaf12d9adab7356223272b656cd0899405
Krish3282 <kvkrishna1938@gmail.com>
2025-03-15 08:42:30 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lggit  border-b">
+        hello
+    </nav>
+  )
+}
+
+export default Navbar
