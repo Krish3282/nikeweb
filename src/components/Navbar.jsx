bfd42a3e8df1b5a0bc86ca5d565e03d0cb066133
Krish3282 <kvkrishna1938@gmail.com>
2025-03-17 13:57:16 +0530

    added nav component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import logo from '../assets/logo.png';
+import {navItems} from '../constants';
+console.log(navItems)
 const Navbar = () => {
   return (
-    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lggit  border-b">
-        hello
+    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg  border-b border-neutral-700/80">
+        
+        <div className="container px-4 mx-auto relative text-sm">
+          <div className="flex items-center justify-center">
+            <div className="flex items-center flex-shrink-0">
+              <img className='h-10 w-10 mr-2' src={logo} alt="logo" />
+              <span className="text-xl tracking-tight">krishna</span>
+            </div>
+            <ul className='hidden lg:flex ml-14 space-x-12'>
+              {navItems.map((item, index) => (
+                <li key={index}>
+                  <a href={item.href}>{item.label}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
     </nav>
   )
 }
